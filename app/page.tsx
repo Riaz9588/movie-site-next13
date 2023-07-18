d0458dfbe4a26d83c1c964eed62a6ea282c560e5
Riaz9588 <mdriaz9588@gmail.com>
2023-07-18 20:17:25 +0600

    new updated now

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default async function Home() {
   // console.log(res.data)
   return (
     <main>
-      <h1 className='h1'>Test Movies API</h1>
+      <h1 className='h1'>Testing Movies API</h1>
       <div className="grid gap-16 grid-cols-fluid p-4">
         {res.data.results.map((movie: any) => (
           <Movie movie={movie} />
